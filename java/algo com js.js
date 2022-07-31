bb3bf5680ba770c9e519917a1906351f374b4798
Natan almeida da silva <natanalmeida040@gmail.com>
2022-07-31 12:01:10 -0300

    cria a index feliz MB.html

---
+++ java/algo com js.js
@@ -0,0 +1,38 @@
+//var y = 0;
+
+//var y = 1;
+//var y = 2;
+
+ // var preço =2;
+
+  // var desconto = 0.2;
+
+//const PRECO = 2;
+
+// var     valor total = preco - desconto;
+
+
+//preço = preço - desconto;
+
+
+//exemplo 
+
+
+//function   soma (){
+
+//return a + b 
+
+
+//}
+
+
+
+//soma(3,5);
+
+
+
+// o return é usado para depois manipular o resultado da função 
+
+// o cons.log e usado para ver o valor dentro dele é ver se estar funcionando 
+
+
